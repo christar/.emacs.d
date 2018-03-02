19bfab0f7aaf73b5648552daad804da6b807c5c3
christar <christar@users.noreply.github.com>
2018-03-03 00:39:01 +0100

    Fork init.el to bootstrap org config

---
+++ init.el
@@ -0,0 +1,59 @@
+;; init -- belak's emacs init.el
+;;
+;;; Commentary:
+;; This file is only to bootstrap into README.org and set up some
+;; basic timing.
+;;
+;;; Code:
+
+;;; Basic Setup:
+;;
+;; This sets up some initial settings which are only used in this
+;; file.  Specifically debugging and some profiling things.
+
+;; Define the start time so we can measure how long loading took
+;; later.
+
+;; package.el adds this automatically, but we handle this in README.org
+;(package-initialize)
+
+(defconst emacs-start-time (current-time))
+
+;; Set the gc-cons-threshold for init so we have some extra memory to
+;; work with. This should be 25M. We also enable debug-on-error and
+;; debug-on-quit to make it easier to debug startup errors.
+(let ((gc-cons-threshold (* 25 1024 1024))
+      (debug-on-error t)
+      (debug-on-quit t))
+
+  ;; The rest of the config is in README.org, so we load org-mode and
+  ;; bootstrap into README.org.
+  (require 'org)
+  (org-babel-load-file
+   (expand-file-name "README.org" user-emacs-directory))
+
+  (garbage-collect))
+
+;; Now that we're done loading everything, print how long it took.
+(when window-system
+  (let ((elapsed (float-time (time-subtract (current-time) emacs-start-time))))
+    (message "Loading %s...done (%.3fs)" load-file-name elapsed))
+
+  (add-hook 'after-init-hook
+            `(lambda ()
+               (let ((elapsed (float-time (time-subtract (current-time) emacs-start-time))))
+                 (message "Loading %s...done (%.3fs) [after-init]"
+                          ,load-file-name elapsed)))
+            t))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (org))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
